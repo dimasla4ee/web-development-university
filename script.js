68df89b19c0ef7a3aec29513d00c4d73d65cb2f2
dekry <de.krylov@vk.com>
2026-04-21 23:10:36 +0300

    feature: Fill player block info

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ function scrollToPlayer(id) {
         }
     );
 }
+
+const links = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll("section[id]");
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                links.forEach(link => link.classList.remove("active"));
+
+                const id = entry.target.id;
+                const activeLink = document.querySelector(`.nav-link[href="#${id}"]`);
+
+                if (activeLink) {
+                    activeLink.classList.add("active");
+                }
+            }
+        });
+    },
+    {
+        rootMargin: "-40% 0px -55% 0px",
+        threshold: 0
+    }
+);
+
+sections.forEach(section => observer.observe(section));
